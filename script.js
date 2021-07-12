e8857eb2fdc8a4ded86ac3402c2720d5276dcdd1
gribashov <gribashovdeveloper@gmail.com>
2021-07-12 22:43:52 +0300

    add simple validation with if else

---
+++ script.js
@@ -0,0 +1,51 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+// show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+// show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+// event listeners
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  if (username.value === "") {
+    showError(username, "Username is required");
+  } else {
+    showSuccess(username);
+  }
+
+  if (email.value === "") {
+    showError(email, "Email is required");
+  } else {
+    showSuccess(email);
+  }
+
+  if (password.value === "") {
+    showError(password, "Password is required");
+  } else {
+    showSuccess(password);
+  }
+
+  if (password2.value === "") {
+    showError(password2, "Password2 is required");
+  } else if (password2.value !== password.value) {
+    showError(password2, "Passwords do not match");
+  } else {
+    showSuccess(password2);
+  }
+});
