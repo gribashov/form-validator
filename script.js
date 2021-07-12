08ccaed19dbbabcdbafeebfd417c1437cd5f0480
gribashov <gribashovdeveloper@gmail.com>
2021-07-12 22:49:03 +0300

    add function isValidEmail

--- script.js
+++ script.js
@@ -19,6 +19,12 @@ function showSuccess(input) {
   formControl.className = "form-control success";
 }
 
+function isValidEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
 // event listeners
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -31,6 +37,8 @@ form.addEventListener("submit", function (e) {
 
   if (email.value === "") {
     showError(email, "Email is required");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Email is not valid");
   } else {
     showSuccess(email);
   }
